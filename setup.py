bfbab00f63e037cc9f2f5a7852fd39ebd8972c07
Vlasov Artem [Lenovo] <root@proscript.ru>
2018-03-10 12:20:14 +0400

    Обновил setup.py для нормальной работы с UTF-8

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
+# coding: utf-8
+
+import codecs
 import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md')) as f:
-    README = f.read()
-
 
 setup(
     name='num2t4ru',
@@ -16,5 +16,5 @@ setup(
     license="Apache License 2.0",
     test_suite='tests',
     packages=find_packages(),
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    long_description=codecs.open(os.path.join(here, 'README.md'), encoding='utf8').read(),
 )
